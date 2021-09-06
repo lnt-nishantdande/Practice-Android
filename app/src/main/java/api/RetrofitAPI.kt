26baa8835d6962bcec2128d20df0b812c638d64c
Nishant Dande <nishant.dande@lntinfotech.com>
2021-09-06 19:04:06 +0530

    Call user fetch api using retrofit library

--- app/src/main/java/api/RetrofitAPI.kt
+++ app/src/main/java/api/RetrofitAPI.kt
@@ -1,19 +1,56 @@
 package api
 
+import kotlinx.coroutines.runBlocking
+import java.net.UnknownHostException
+
 
 // TODO: Implement a simple networking API using retrofit or any library you're familiar with 🙌
 // Note: Fetching users-list is the *only* use-case therefore avoid implementing unrelated,
 // extra, features no matter how common or useful they might be for potential future cases.
 //
-class RetrofitAPI(private val usersListURL: String) : API {
+class RetrofitAPI() : API {
+
+    /**
+     * Declare and Initialize user api service lazily whenever required
+     */
+    private val userApiService by lazy {
+        API.create()
+    }
 
     override fun fetchUsersList(
         excludingUserWithID: String?,
         success: (UsersList) -> Unit,
         failure: (FetchError) -> Unit
     ) {
-        print("Fetch users list from $usersListURL")
-        TODO("Not yet implemented")
+        // As we are making requesting using 'async',
+        // call must be made from coroutine hence api request
+        // is made from 'runBlocking' coroutine builder.
+        runBlocking {
+            // Request to get data from 'users' endpoint
+            val req = userApiService.fetchUsersList()
+            try {
+                // wait for response and handle api response using 'success' or 'failure' functions
+                req.await().run {
+                    if (this != null){
+                        // Handle success response via 'success' function
+                        success.invoke(UsersList(this))
+                    } else {
+                        // Handle error response via 'failure' function
+                        failure.invoke(FetchError(IllegalArgumentException(), "No Data Found"))
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                // Case to handle various network errors
+                if (e is UnknownHostException){
+                    // handle case when user has no internet connection
+                    failure.invoke(FetchError(e, "Internet Not Available"))
+                } else {
+                    // handle various general network cases
+                    failure.invoke(FetchError(e, e.localizedMessage))
+                }
+            }
+        }
     }
 
 }
